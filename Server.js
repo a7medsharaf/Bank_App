c0c3a8ce81b71626548ab27ca41fc4c2f18db437
a7medsharaf <a7med.sharaf@gmail.com>
2021-10-14 20:53:49 +0200

    r

--- Server.js
+++ Server.js
@@ -1,4 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var Transactions_Server_1 = require("./dist/Servers/Transactions_Server");
-var TS = new Transactions_Server_1.Transactions_Server(8888);
+console.log(process.env.PORT);
+var TS = new Transactions_Server_1.Transactions_Server(process.env.PORT);
